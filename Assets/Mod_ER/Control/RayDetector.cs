3231698e7749b925d9539d5e1d86abceec866d95
Marisa23356 <710871626@qq.com>
2023-07-27 20:52:03 +0800

    更新UI工具包

---
+++ Assets/Mod_ER/Control/RayDetector.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ER.Control
+{
+    /// <summary>
+    /// 射线检测器
+    /// </summary>
+    public class RayDetector:MonoBehaviour
+    {
+
+    }
+}
