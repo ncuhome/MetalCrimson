ffdbd06e0aafdc194ae4b3459ee19c99ec3cd67c
Marisa23356 <710871626@qq.com>
2023-08-05 21:19:01 +0800

    武器动画同步脚本，同时完善了一些 Action的封装，添加了 基础的角色控件检测器

--- Assets/Mod_ER/Entity2D/Attribute/ATAction.cs
+++ Assets/Mod_ER/Entity2D/Attribute/ATAction.cs
@@ -3,37 +3,37 @@ using UnityEngine;
 namespace ER.Entity2D
 {
     /// <summary>
-    /// 对角色动作的封装
+    /// 对角色动作的封装，隶属于 ATActionManager 下
     /// </summary>
-    public class ATAction : MonoAttribute
+    public abstract class MDAction:MonoBehaviour
     {
 
         #region 初始化
-        public ATAction() { AttributeName = nameof(ATAction); }
-        public override void Initialize()
-        {
-            
-        }
+        public MDAction() { actionName = "Unkown"; }
+        /// <summary>
+        /// 动作被加载进 ATActionManager 后的初始化
+        /// </summary>
+        public abstract void Initialize();
         #endregion
 
 
         #region 属性
+
+        /// <summary>
+        /// 动作名称
+        /// </summary>
+        public string actionName;
+        /// <summary>
+        /// 所属动作集合对象
+        /// </summary>
+        public ATActionManager manager;
         /// <summary>
         /// 动作索引
         /// </summary>
-        private int index;
-        /// <summary>
-        /// 动作有效区域（动作可以无有效区域，例如移动）
-        /// </summary>
-        private ATActionRegion region;
-        /// <summary>
-        /// 动作自身的动画机
-        /// </summary>
-        private Animator animator;
+        public int index;
         #endregion
 
         #region 功能
-        
         #endregion
     }
 }
--- Assets/Mod_ER/Entity2D/Attribute/ATActionManager.cs
+++ Assets/Mod_ER/Entity2D/Attribute/ATActionManager.cs
@@ -1,25 +1,68 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
 namespace ER.Entity2D
 {
     /// <summary>
-    /// 对角色动作的封装
+    /// 对角色动作的管理
     /// </summary>
     public class ATActionManager : MonoAttribute
     {
-
         #region 初始化
-        public ATActionManager() { AttributeName = nameof(ATActionManager); }
+
+        public ATActionManager()
+        { AttributeName = nameof(ATActionManager); }
+
         public override void Initialize()
         {
-
         }
-        #endregion
 
+        #endregion 初始化
 
         #region 属性
+        /// <summary>
+        /// 动作预加载栏
+        /// </summary>
+        public List<MDAction> _actions = new List<MDAction>();
 
+        /// <summary>
+        /// 角色拥有的动作集合
+        /// </summary>
+        private Dictionary<string, MDAction> actions = new();
 
+        /// <summary>
+        /// 角色自身的动画机
+        /// </summary>
+        private Animator animator;
+
+        #endregion 属性
+
+        #region 动作管理
+        public void Add(MDAction action)
+        {
+            if(actions.ContainsKey(action.actionName))
+            {
+                Debug.LogError("该动作槽位已经被占用");
+            }
+            action.manager = this;
+            action.index = actions.Count;
+            actions[action.actionName] = action;
+            action.Initialize();
+        }
+
+        /// <summary>
+        /// 触发角色动作
+        /// </summary>
+        /// <param name="actionName">动作名称</param>
+        public void Action(string actionName)
+        {
+            if (actions.TryGetValue(actionName, out MDAction action))
+            {
+                animator.SetInteger("act", action.index);
+                return;
+            }
+            Debug.LogError($"未找到指定动作：{actionName}");
+        }
         #endregion
     }
 }
