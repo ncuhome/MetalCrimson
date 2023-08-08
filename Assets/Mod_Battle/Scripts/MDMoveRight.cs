46afb252aa69403c98832cc69f82e856a3e72459
Marisa23356 <710871626@qq.com>
2023-08-08 15:20:06 +0800

    添加 基础的动作脚本（未完善），添加新特征 buff栏

---
+++ Assets/Mod_Battle/Scripts/MDMoveRight.cs
@@ -0,0 +1,13 @@
+﻿using ER.Entity2D;
+
+namespace Mod_Battle
+{
+    public class MDMoveRight : MDAction
+    {
+        public MDMoveRight() { actionName = "右移动"; }
+        public override void Initialize()
+        {
+
+        }
+    }
+}
