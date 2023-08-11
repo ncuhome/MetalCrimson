10a8de4012b4179babaf31391c07ada01d88026f
Marisa23356 <710871626@qq.com>
2023-08-11 13:29:27 +0800

    添加UIBar，添加新的角色属性

---
+++ Assets/Mod_Battle/Scripts/ATWeaponState.cs
@@ -0,0 +1,53 @@
+﻿namespace ER.Entity2D
+{
+    /// <summary>
+    /// 武器状态
+    /// </summary>
+    public class ATWeaponState : MonoAttribute
+    {
+        #region 初始化
+
+        public ATWeaponState()
+        { AttributeName = nameof(ATWeaponState); }
+
+        public override void Initialize()
+        {
+        }
+
+        #endregion 初始化
+
+        #region 属性
+
+        /// <summary>
+        /// 基础质量
+        /// </summary>
+        public float defWeight;
+
+        /// <summary>
+        /// 基础锋利度
+        /// </summary>
+        public float defSharpness;
+
+        /// <summary>
+        /// 基础耐久度
+        /// </summary>
+        public float defDurability;
+
+        /// <summary>
+        /// 修正质量
+        /// </summary>
+        public float weight;
+
+        /// <summary>
+        /// 修正锋利度
+        /// </summary>
+        public float sharpness;
+
+        /// <summary>
+        /// 修正耐久度
+        /// </summary>
+        public float durability;
+
+        #endregion 属性
+    }
+}
