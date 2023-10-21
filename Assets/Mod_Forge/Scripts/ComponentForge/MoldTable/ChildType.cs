4c5f529a12273f640a96280ec59eb3245f5a5935
FeatherRian <1178441170@qq.com>
2023-10-21 22:35:17 +0800

    🐛 修复信息模板出现不正确的bug

--- Assets/Mod_Forge/Scripts/ComponentForge/MoldTable/ChildType.cs
+++ Assets/Mod_Forge/Scripts/ComponentForge/MoldTable/ChildType.cs
@@ -24,6 +24,7 @@ public class ChildModelType : MonoBehaviour, IPointerClickHandler, IPointerEnter
     public Color targetColor;
     public float colorTime;
     public bool startColor;
+    public bool showChildModel;
 
     // Start is called before the first frame update
     void Start()
@@ -72,6 +73,8 @@ public class ChildModelType : MonoBehaviour, IPointerClickHandler, IPointerEnter
                 colorTime = 0;
             }
         }
+
+        showChildModel = (typeImage.color.a != 0);
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -95,16 +98,19 @@ public class ChildModelType : MonoBehaviour, IPointerClickHandler, IPointerEnter
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (!showChildModel) { return; }
         TypeSystem.Instance.ShowCard(TypeSystem.Instance.GetChildType(motherId, id));
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (!showChildModel) { return; }
         TypeSystem.Instance.HideCard();
     }
 
     public void OnPointerMove(PointerEventData eventData)
     {
+        if (!showChildModel) { return; }
         Vector2 pos;
         RectTransform rectTransform = UIManager.Instance.canvas.GetComponent<RectTransform>();
         RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.enterEventCamera, out pos);
