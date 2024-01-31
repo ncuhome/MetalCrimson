a9e97b46464e832004a4187b441672690557f5af
Marisa23356 <710871626@qq.com>
2024-01-31 18:03:20 +0800

    更新标签, 修复传送门失效的bug

---
+++ Assets/Mod_Level/Scripts/Interact/InteractUIPanel.cs
@@ -0,0 +1,21 @@
+﻿using ER;
+using System.Transactions;
+
+namespace Mod_Level
+{
+    /// <summary>
+    /// 交互UI显示管理面板
+    /// </summary>
+    public class InteractUIPanel:MonoSingleton<InteractUIPanel>
+    {
+        public InteractObject nowInteractObject = null;
+        public void DisplayUI(InteractObject obj, UIInfo info)
+        {
+            if(nowInteractObject!=null)
+            {
+
+            }
+        }
+    }
+
+}
