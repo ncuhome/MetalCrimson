df90deadc2920fbee55a8c624138827c170274e1
Marisa23356 <710871626@qq.com>
2024-01-31 17:29:00 +0800

    修复Box预制体错误脚本

---
+++ Assets/Mod_Level/Scripts/Interact/InteractObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Mod_Level
+{
+    /// <summary>
+    /// 可交互物体
+    /// </summary>
+    public class InteractObject : MonoBehaviour, IInteractive
+    {
+        public void EnterInteract()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ExitInteract()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
