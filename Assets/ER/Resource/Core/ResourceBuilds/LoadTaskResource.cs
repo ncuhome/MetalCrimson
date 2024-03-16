53cb34e8e557035b06ed244c73bec8e3512b1bff
Marisa23356 <710871626@qq.com>
2024-03-16 15:04:19 +0800

    导入 ER.Resource 0.5.0

---
+++ Assets/ER/Resource/Core/ResourceBuilds/LoadTaskResource.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+namespace ER.Resource
+{
+    /// <summary>
+    /// 音频资源
+    /// </summary>
+    public class LoadTaskResource : IResource
+    {
+        private string registryName;
+        private LoadTask task;
+        public string RegistryName { get => registryName; }
+        /// <summary>
+        /// AudioClip 资源对象
+        /// </summary>
+        public LoadTask Value => task;
+
+        public LoadTaskResource(string _registryName, string json)
+        {
+            registryName = _registryName;
+            task = JsonConvert.DeserializeObject<LoadTask>(json);
+        }
+    }
+}
